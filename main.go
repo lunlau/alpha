aa8802f37d21efe99d3db63528d37e6909c786f5
andyklliu <>
2022-01-08 03:09:22 +0800

    增加http代理服务

--- main.go
+++ main.go
@@ -66,80 +66,14 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	resp,_ := client.Do(req)
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Printf(string(body))
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET,PUT,DELETE,POST,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.WriteHeader(200)
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-	w.Header().Add("Access-Control-Allow-Methods", "GET,PUT,DELETE,POST,OPTIONS")
-	w.Header().Add("Access-Control-Allow-Credentials", "true")
-	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Write(body)
 	return
 }
-
-func indexHandlerBak(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "gRPC client")
-	fmt.Println("start index")
-	rspJson := make(map[int]string,0)
-	rspJson[0] = "success"
-	msg, _ := json.Marshal(rspJson)
-	w.Header().Set("content-type","text/json")
-	w.WriteHeader(200)
-	w.Write(msg)
-
-	getDataByUrl(w,r)
-	fmt.Println("end")
-	return
-}
-
-
-//
-//curl 'http://52.41.98.206:3000/api/auth/keys?includeExpired=false' \
-//  -H 'Connection: keep-alive' \
-//  -H 'Pragma: no-cache' \
-//  -H 'Cache-Control: no-cache' \
-//  -H 'accept: application/json, text/plain, */*' \
-//-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36' \
-//-H 'x-grafana-org-id: 1' \
-//-H 'Referer: http://52.41.98.206:3000/org/apikeys' \
-//-H 'Accept-Language: zh-CN,zh-TW;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6' \
-//-H 'Cookie: grafana_session_3000=fd65002d5e5734061c774c8c3e1825a0' \
-//--compressed \
-
-//
-//
-//curl 'http://52.41.98.206:3000/api/auth/keys?includeExpired=false' \
-//-H 'Connection: keep-alive' \
-//-H 'Pragma: no-cache' \
-//-H 'Cache-Control: no-cache' \
-//-H 'accept: application/json, text/plain, */*' \
-//-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36' \
-//-H 'x-grafana-org-id: 1' \
-//-H 'Referer: http://52.41.98.206:3000/org/apikeys' \
-//-H 'Accept-Language: zh-CN,zh-TW;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6' \
-//-H 'Cookie: grafana_session_3000=fd65002d5e5734061c774c8c3e1825a0' \
-//--compressed \
-//
-
-func getDataByUrl(w http.ResponseWriter, r *http.Request)  {
-	//
-	client := &http.Client{}
-	req,_ := http.NewRequest("GET","http://52.41.98.206:3000/api/auth/keys?includeExpired=false",nil)
-	//req.Header.Add("Authorization","Bearer eyJrIjoiR0FKRFF5S1F4aFlNSUFjNlVYQ3JUZ2N1azdNWWNaNDMiLCJuIjoiaGFuY2tzaG9uMiIsImlkIjoxfQ==\" http://52.41.98.206:3000/api/dashboards/home")
-
-	req.Header.Add("accept", "application/json, text/plain, */*")
-	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36")
-	req.Header.Add("x-grafana-org-id", "1")
-	req.Header.Add("Referer", "http://52.41.98.206:3000/org/apikeys")
-	req.Header.Add("Accept-Language", "zh-CN,zh-TW;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6")
-	req.Header.Add("Cookie", "grafana_session_3000=fd65002d5e5734061c774c8c3e1825a0")
-	resp,_ := client.Do(req)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Printf(string(body))
-
-	return
-}
-
-
-
 
 //定义conf类型
 //类型里的属性，全是配置文件里的属性
